81eea80cc8ab851091682b5604fdd98347a1f30d
Anthony <niklausantonio@gmail.com>
2025-08-12 20:44:39 +0100

    db config

--- backend/app/db.py
+++ backend/app/db.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app: Flask):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:insight_pass@db:5432/insightdb"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://insight_user:insight_pass@db:5432/insightdb"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     with app.app_context():
